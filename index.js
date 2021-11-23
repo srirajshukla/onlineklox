fda8e23671bdc7065a3c737d5c19f28a5f877a7c
Gyanendra Shukla <srirajshukla@gmail.com>
2021-11-23 18:48:21 +0530

    it works!

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 3156;
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function(ws) {
+  console.log("new connection?");
+  startShell(ws)
+});
+
+const os = require('os');
+const pty = require('node-pty');
+
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+function startShell(ws) {
+  console.log("A new client connected");
+
+  const ptyProcess = pty.spawn(shell, ["java", "-jar", ".\\Klox.jar"], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env
+  });
+
+  console.log("PTY process created");
+
+  ptyProcess.onData(function(data) {
+    ws.send(data);
+  });
+
+  ws.on('message', function(msg) {
+    ptyProcess.write(msg);
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
+
---
+++ prebuildscript.sh
@@ -0,0 +1,5 @@
+echo "\nDownloading latest release ... \n"
+wget https://github.com/srirajshukla/klox/releases/download/statement_evaluation/Klox.jar
+echo "\n Successfully downloaded Klox"
+mv Klox.jar ~/
+echo "\n Successfully moved Klox.jar to home directory"
