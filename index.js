28cbff8dbeba395d8675757e354f4ee633790a46
Gyanendra Shukla <srirajshukla@gmail.com>
2021-11-23 19:25:29 +0530

    just run java

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const PORT = process.env.PORT || 3156;
-
+const homed = process.env.HOMED || "d:\\workspace\\onlineklox\\klox\\";
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function(ws) {
@@ -19,14 +19,14 @@ const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 function startShell(ws) {
   console.log("A new client connected");
 
-  const ptyProcess = pty.spawn(shell, ["java", "-jar", ".\\Klox.jar"], {
+  const ptyProcess = pty.spawn(shell, ["java"], {
     name: 'xterm-color',
     cols: 80,
     rows: 30,
     cwd: process.env.HOME,
     env: process.env
   });
-
+  console.log(process.env.HOMEDD)
   console.log("PTY process created");
 
   ptyProcess.onData(function(data) {
