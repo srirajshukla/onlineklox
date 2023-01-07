6ddf3ad5a6e5bf72ffc2357243c615e9d6143f3e
Gyanendra Shukla <srirajshukla@gmail.com>
2023-01-07 21:50:18 +0530

    feat: move script from index.html to new file

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import {Terminal} from "xterm";
+import './style.css'
+
+var ws = new WebSocket(`ws://localhost:3156`);
+
+console.log("created instace of websocket");
+console.log(ws);
+
+var term = new Terminal();
+term.open(document.getElementById("terminal"));
+
+term.write("Try out \x1B[1;3;31mKlox\x1B[0m \r\n");
+
+term.write(
+  "Contribute to the project at: https://github.com/srirajshukla/klox \r\n"
+);
+
+
+// term.write("$ ");
+term.onData((e) => {
+  ws.send(e);
+});
+
+ws.onmessage = function (e) {
+  term.write(e.data);
+};
+
